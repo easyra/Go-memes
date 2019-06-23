12ce279651c482af11f214c2cb42aef7664f4627
ezra davis davis <ezra.rr.davis@gmail.com>
2019-06-23 10:29:15 -0700

    const printing section completed

--- Variables/const/main.go
+++ Variables/const/main.go
@@ -2,5 +2,21 @@ package main
 
 import "fmt"
 
-//Const:
-//Can
+const (
+  Pi = 3.14
+  Truth= false
+  Big = 1<< 62
+  small = Big >> 61
+)
+
+func main(){
+  const Greeting = "Hi"
+  fmt.Println(Greeting)
+  fmt.Println(Pi)
+  fmt.Println(Truth)
+	fmt.Println(Big)
+	name := "Caperica-Six"
+  aka := fmt.Sprintf("Number %d", 6)
+  fmt.Printf("%s is also known as %s",
+    name,aka)
+}
